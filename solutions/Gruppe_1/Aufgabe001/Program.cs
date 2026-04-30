3bf90f614b02ddd6caa63ebc821bc83f168519ec
Volker Goehler <volker.goehler@informatik.tu-freiberg.de>
2026-04-30 16:03:21 +0200

    added solution for second exercise, these are only parts as we just touched the first task

---
+++ solutions/Gruppe_1/Aufgabe001/Program.cs
@@ -0,0 +1,192 @@
+﻿public enum Typ
+{
+    Stern,
+    Planet,
+    Mond
+}
+
+
+public class Himmelskoerper
+{
+    private string _name;
+    public string Name 
+    { 
+        get => _name;
+        set => _name = value.Trim();
+    }
+    private uint _katnr;
+    public uint KatalogNummer { 
+        get => _katnr;
+        set {
+           if(!isAKatalogNummer(value))
+           {
+                throw new ArgumentOutOfRangeException(nameof(value), "Katalognummer muss 5-stellig sein.");
+           }
+           _katnr = value;
+        }
+    }
+    // typ
+    public Typ Himmelskoerpertyp { get; set;}
+    // spectral
+    private char? _spectralklasse;
+
+    public char? Spectral { 
+        get => _spectralklasse;
+        set {
+            if (value != null)
+            {
+                char[] gueltigeSpektralklassen = { 'O', 'B', 'A', 'F', 'G', 'K', 'M'};
+                if (!gueltigeSpektralklassen.Contains(value.Value))
+                {
+                    throw new ArgumentOutOfRangeException(
+                        nameof(value),
+                        "Ungültige Spektralklasse."
+                    );
+                }
+            }
+            _spectralklasse = value;
+
+        }
+    }
+    // helligkeit
+    private float? _helligkeit;
+    public float? helligkeit {
+         get => _helligkeit;
+         set {
+            if (!isNullorSmaller(value)){
+                _helligkeit = value;
+            }else{
+                throw new ArgumentOutOfRangeException(nameof(value), "Helligkeit muss positiv sein.");
+            }
+         }
+    }
+    // Umlauf
+    private float? _umlauf;
+    public float? umlauf {
+        get => _umlauf;
+        set {
+            if (!isNullorSmaller(value)){
+                _umlauf = value;
+            }else{
+                throw new ArgumentOutOfRangeException(nameof(value), "Umlaufzeit muss positiv sein.");
+            }
+
+        }
+    }
+    // KatNr von Elternobjekt
+    private uint _katnrref;
+    public uint KatalogNummerReferenz {
+        get => _katnrref;
+        set {
+           if(isAKatalogNummer(value))
+           {
+                throw new ArgumentOutOfRangeException(nameof(value), "Katalognummer muss 5-stellig sein.");
+           }
+           _katnrref = value;
+        }
+    }    
+
+    private static bool isAKatalogNummer(uint katnr)
+    {
+        return !(katnr < 10000 || katnr > 99999);
+    }
+
+    private static bool isNullorSmaller(float? zahl)
+    {
+        return !zahl.HasValue || zahl <= 0;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Himmelskoerper koerper = new Himmelskoerper();
+        try
+        {
+            LeseEin(koerper, args);
+        }
+        catch (System.Exception)// speziellen Fehler behandeln
+        {
+            // hier die Behandlung der Fehler
+            throw;
+        }
+        Ausgabe(koerper);
+    }
+
+    public static void Ausgabe(Himmelskoerper koerper)
+    {
+        string output = $"Himmelskörper: {koerper.Name}, Katalog-Nummer: {koerper.KatalogNummer}, Typ:{koerper.Himmelskoerpertyp}";
+        if (koerper.Himmelskoerpertyp == Typ.Stern)
+        {
+            Console.WriteLine($"{output}, Spektralklasse: {koerper.Spectral}, scheinbare Helligkeit: {koerper.helligkeit}");
+        }else{
+            Console.WriteLine($"{output}, Umlaufzeit: {koerper.umlauf} Erdjahren, Zentralkörper-Katalog-Nummer: {koerper.KatalogNummerReferenz}");
+        }
+    }
+                    
+    public static void InteraktiveEingabe(Dictionary<string, string> dict, string[] output, int offset = 0)
+    {
+            foreach (var (key, values) in dict)
+            {
+                // ausgabe
+                Console.WriteLine(values);
+                // eingabe
+                int idx = Array.IndexOf([.. dict.Keys], key) + offset;
+                output[idx] = Console.ReadLine();
+            }
+    }
+
+    public static void LeseEin(Himmelskoerper koerper, string[] args)
+    {
+        if (args.Length > 0)
+        {
+            SetKoerper(koerper, args);
+        }else{
+            Dictionary<string, string> anfragen = new()
+            {
+                ["Name"] = "Name des Himmelskörpers:",
+                ["KatalogNummer"] = "KatalogNummer (5-stellig):",
+                ["Typ"] = "Geben Sie den Typ ein (Stern, Planet, Mond):"
+            };
+            Dictionary<string, string> anfragenStern = new()
+            {
+                ["Spektralklasse"] = "Spektralklasse (O, B, A, F, G, K, M): ",
+                ["ScheinbareHelligkeit"] = "scheinbare Helligkeit: "
+            };
+            Dictionary<string, string> anfragenPlanetMond = new()
+            {
+                ["Umlaufzeit"] = "Umlaufzeit in Erdjahren: ",
+                ["ZentralkoerperKatalogNummer"] = "KatalogNummer des Zentralkörpers: "
+            };
+
+            string[] output = new string[5];
+            InteraktiveEingabe(anfragen, output);
+
+            if (output[2] == Typ.Stern.ToString()){
+                // eingabe für Stern
+                InteraktiveEingabe(anfragenStern, output, 3);
+            } else {
+                // eingabe für Planet/Mond
+                InteraktiveEingabe(anfragenPlanetMond, output, 3);
+            }
+            SetKoerper(koerper, output);
+        }
+
+    }
+
+    public static void SetKoerper(Himmelskoerper koerper, string[] input){
+        koerper.Name = input[0];
+        koerper.KatalogNummer = uint.Parse(input[1]);
+        koerper.Himmelskoerpertyp = (Typ) Enum.Parse(typeof(Typ), input[2]);
+        if (koerper.Himmelskoerpertyp == Typ.Stern)
+        {
+            koerper.Spectral = input[3][0];
+            koerper.helligkeit = float.Parse(input[4]);
+        }else{
+            koerper.umlauf = float.Parse(input[3]);
+            koerper.KatalogNummerReferenz = uint.Parse(input[4]);
+        }
+    }
+
+}
---
+++ solutions/Gruppe_2/Aufgabe001/Program.cs
@@ -0,0 +1,217 @@
+﻿public enum HimmelskoerperTyp
+{
+    Stern,
+    Planet,
+    Mond,
+    Todestern
+}
+
+public class Himmelskoerper
+{
+    private string _name;
+    public string Name
+    {
+        get => _name;
+        set => _name = value.Trim();
+    }
+    // katalognummer
+    private uint _katalogNr;
+    public uint KatalogNummer
+    {
+        get => _katalogNr;
+        set
+        {
+            // check
+            if (isCatalogueNumberNotValid(value))
+            {
+                throw new ArgumentOutOfRangeException(nameof(value), "Katalognummer nicht gültig (5 Zeichen).");
+            }
+            // zuweisung
+            _katalogNr = value;
+        }
+    }
+    // HimmelskoerperTyp
+    public HimmelskoerperTyp Typ { get; set; }
+    // Spektralklasse
+    private char? _spectralKlasse;
+    public char? SpektralKlasse
+    {
+        get => _spectralKlasse;
+        set
+        {
+            if (value != null)
+            {
+                char[] gueltigeSpektralKlassen = ['O', 'B', 'A', 'F', 'G', 'K', 'M'];
+                if (!gueltigeSpektralKlassen.Contains(value.Value))
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), "Spektralklasse muss eine von (O, B, A, F, G, K, M) sein");
+                }
+            }
+            _spectralKlasse = value;
+        }
+    }
+    // ScheinbareHelligkeit
+    private double? _scheinbareHelligkeit;
+    public double? ScheinbareHelligkeit { 
+        get => _scheinbareHelligkeit;
+        set{
+            if (isDoubleSmallerZero(value))
+            {
+                throw new ArgumentOutOfRangeException(nameof(value), "Scheinbare Helligkeit darf nicht kleiner als 0 sein.");
+            }
+            _scheinbareHelligkeit = value;
+        } 
+    }
+    // Umlaufzeit
+    private double? _umlaufzeit;
+    public double? Umlaufzeit { 
+        get => _umlaufzeit; 
+        set{
+            if (isDoubleSmallerZero(value))
+            {
+                throw new ArgumentOutOfRangeException(nameof(value), "Umlaufzeit darf nicht 0 oder weniger sein.");
+            }
+            _umlaufzeit = value;
+        } 
+    }
+
+    // ZentralkoerperKatalogNummer
+    private uint? _zentralkoerperKatalogNummer;
+    public uint? ZentralkoerperKatalogNummer { 
+        get => _zentralkoerperKatalogNummer;
+        set 
+        {
+            if (isCatalogueNumberNotValid(value))
+            {
+                throw new ArgumentOutOfRangeException(nameof(value), "Katalognummer des Zentralgestirns nicht gültig (5 Zeichen).");
+            }
+            _zentralkoerperKatalogNummer = value;
+        }
+    }
+
+    public float? UmlaufzeitInTagen {
+        get {
+            if (Umlaufzeit.HasValue)
+            {
+                return (float)(Umlaufzeit.Value * 365.25);
+            }
+            return null;
+        }
+    }
+
+    public string VollstaendigerTyp {
+        get {
+            if (Typ == HimmelskoerperTyp.Stern)
+            {
+                return $"{Typ} ({SpektralKlasse}-Klasse)";
+            }
+            return Typ.ToString();
+        }
+    }
+
+    private static bool isCatalogueNumberNotValid(uint? number)
+    {
+        if (number != null)
+        {
+            return number < 10000 || number > 99999;
+        }
+        return false;
+    }
+    private static bool isDoubleSmallerZero(double? number)
+    {
+        return number != null && number <= 0;
+    }
+}
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Himmelskoerper stern = new Himmelskoerper();
+        LeseDatenEin(stern, args);
+        GibDatenAus(stern);
+    }
+
+    public static void GibDatenAus(Himmelskoerper koerper)
+    {
+        Console.WriteLine("Himmelskörper:");
+        Console.WriteLine($"Name: {koerper.Name}, Katalog-Nummer: {koerper.KatalogNummer}, Typ: {koerper.VollstaendigerTyp}");
+        if (koerper.Typ == HimmelskoerperTyp.Stern)
+        {
+            Console.WriteLine($"Scheinbare Helligkeit: {koerper.ScheinbareHelligkeit}");
+        }
+        else
+        {
+            Console.WriteLine($"Umlaufzeit: {koerper.Umlaufzeit} Erdjahre ({koerper.UmlaufzeitInTagen} Erdtage), Zentralkörper-Katalognummer: {koerper.ZentralkoerperKatalogNummer}");
+        }
+    }
+
+    private static void WerteEingabe(Himmelskoerper koerper, string[] input){
+        koerper.Name = input[0];
+        koerper.KatalogNummer = uint.Parse(input[1]);
+        koerper.Typ = (HimmelskoerperTyp)Enum.Parse(typeof(HimmelskoerperTyp), input[2]);
+
+        if (koerper.Typ == HimmelskoerperTyp.Stern)
+        {
+            // SpektralKlasse
+            koerper.SpektralKlasse = input[3][0]; //char.Parse(args[3])
+            // ScheinbareHelligkeit
+            koerper.ScheinbareHelligkeit = double.Parse(input[4]);
+        }
+        else
+        {
+            // Umlaufzeit
+            koerper.Umlaufzeit = double.Parse(input[3]);
+            // ZentralkoerperKatalogNummer
+            koerper.ZentralkoerperKatalogNummer = uint.Parse(input[4]);
+        }
+    }
+            
+    private static void LoopDictionary(Dictionary<string, string> dict, string[] output, int offset = 0){
+        foreach (var (key, values) in dict)
+        {
+            Console.WriteLine(values);
+            int idx = Array.IndexOf([.. dict.Keys], key) + offset;
+            output[idx] = Console.ReadLine();
+        }
+    }
+
+    public static void LeseDatenEin(Himmelskoerper koerper, string[] args)
+    {
+        if (args.Length > 1)
+        {
+            WerteEingabe(koerper, args);
+        }
+        else
+        {
+            Dictionary<string, string> anfragen = new(){
+                ["Name"] = "Namen des Himmelskörpers: ",
+                ["KatalogNummer"] = "Katalognummer (5-Stellig): ",
+                ["Typ"] = "Himmelskörpertyp (Stern, Planet, Mond): ",
+            };
+            Dictionary<string, string> anfragenStern = new(){
+                ["Spektralklasse"] = "Spektralklasse",
+                ["Helligkeit"] = "Scheinbare Helligkeit",
+            };
+            Dictionary<string, string> anfragenPlanet = new(){
+                ["Umlauf"] = "Umlaufzeit in Jahren",
+                ["KatNrZentral"] = "Katalognummer des Zentralgestirns: "
+            };
+
+            string[] output = new string[5];
+
+            LoopDictionary(anfragen, output);
+            if (output[2] == HimmelskoerperTyp.Stern.ToString()){
+                LoopDictionary(anfragenStern, output, 3);
+            }else
+            {
+                LoopDictionary(anfragenPlanet, output, 3);
+            }
+
+            WerteEingabe(koerper, output);
+        }
+    }
+
+    //public static Himmelskoerper LeseDatenEin(string[] args){}
+}
